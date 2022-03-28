606a821a1e11931168d5bcf258890e5ba633d313
tigernoobs-1 <70248532+tigernoobs-1@users.noreply.github.com>
2022-03-28 09:57:21 +0800

    First Commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,52 @@
+class TodosController < ApplicationController
+	
+	before_action :set_todo, only: [:show, :update, :destroy]
+
+	def index
+		@todos = Todo.all
+		render json: @todos
+	end
+
+	def show
+
+		render json: @todo
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+
+		if @todo.save
+			render json: @todo
+
+		else
+			render json: @todo.errors, status: :errors_item
+
+		end
+
+	end
+
+	def update
+		if @todo.update(todo_params)
+			render json: @todo
+
+		else
+			render json: @todo.errors, status: :errors_item
+		end
+
+	end
+
+	def destroy
+
+		@todo.destroy
+
+	end
+
+	private
+		def set_todo
+			@todo= Todo.find(params[:id])
+		end
+
+		def todo_params
+			params.require(:todo).permit(:id, :title, :completed)
+		end
+end
